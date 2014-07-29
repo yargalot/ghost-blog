7a1f3db67b080c05735882ffa0e88d579dc41ed3
Steven John Miller <steven.jmiller@gmail.com>
2014-07-29 20:28:48 +1000

    Init commit

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '8081'
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://stevenjohnmiller.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -61,7 +61,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '8081'
         }
     },
 
